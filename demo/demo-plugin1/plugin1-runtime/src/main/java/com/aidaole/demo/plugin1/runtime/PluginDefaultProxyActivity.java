50f0f9910680f7aa76ab6fe60f729cc2ab8048d2
aidaole <aidaole@outlook.com>
2025-03-17 13:40:57 +0800

    feature: 添加plugin1-loader 和 plugin1-runtime

---
+++ demo/demo-plugin1/plugin1-runtime/src/main/java/com/aidaole/demo/plugin1/runtime/PluginDefaultProxyActivity.java
@@ -0,0 +1,7 @@
+package com.aidaole.demo.plugin1.runtime;
+
+
+import com.tencent.shadow.core.runtime.container.PluginContainerActivity;
+
+public class PluginDefaultProxyActivity extends PluginContainerActivity {
+}
---
+++ demo/demo-plugin1/plugin1-runtime/src/main/java/com/aidaole/demo/plugin1/runtime/PluginSingleInstance1ProxyActivity.java
@@ -0,0 +1,7 @@
+package com.aidaole.demo.plugin1.runtime;
+
+
+import com.tencent.shadow.core.runtime.container.PluginContainerActivity;
+
+public class PluginSingleInstance1ProxyActivity extends PluginContainerActivity {
+}
